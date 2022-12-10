b6ab2423e5a7dca23ffc8fadbe81b1fbef86eaf3
TomasFuentesA <tomas.fuentes@mail.udp.cl>
2022-12-10 00:16:04 -0300

    login start

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, request, jsonify, redirect
+from flask import Flask, request, jsonify, redirect, render_template
+
 import sqlite3
 import os
 
-DATABASE_NAME = 'IoT.db'
-
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return redirect("http://www.example.com", code=302)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        mail = request.form.get('mail')
+        password = request.form.get('password')
+
+        return jsonify({'mail': mail, 'password': password})
 
 if __name__ == '__main__':
     app.run()
