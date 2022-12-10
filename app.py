2fffcdf3ffc02e726c6ec12bdffa279669a3d070
EstebanAlarconO <esteban.alarcon1@mail.udp.cl>
2022-12-10 00:38:02 -0300

    login

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify, redirect, render_template
+from backend.service_login import service_login
 
 import sqlite3
 import os
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def main():
+    return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,8 +18,7 @@ def login():
     else:
         mail = request.form.get('mail')
         password = request.form.get('password')
-
-        return jsonify({'mail': mail, 'password': password})
-
-if __name__ == '__main__':
-    app.run()
+        if (service_login(mail, password)):
+            return jsonify({'mail': mail, 'password': password})
+        else:
+            return 0
