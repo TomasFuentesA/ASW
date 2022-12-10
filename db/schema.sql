af5cdb105a28beae903908830085336238335675
TomasFuentesA <tomas.fuentes@mail.udp.cl>
2022-12-10 03:57:51 -0300

    SCHEMA FIX

--- db/schema.sql
+++ db/schema.sql
@@ -3,41 +3,41 @@ DROP TABLE IF EXISTS paciente;
 DROP TABLE IF EXISTS diagnostico_medico;
 
 CREATE TABLE cuenta(
-   id_cuenta SERIAL PRIMARY KEY,
-   correo varchar(255) NOT NULL,
-   contrasena varchar(255) NOT NULL,
-   nombre_s varchar(255) NOT NULL,
-   apellido_s varchar(255) NOT NULL,
-   especialidad varchar(255),
+   id_cuenta INTEGER PRIMARY KEY AUTOINCREMENT,
+   correo TEXT UNIQUE NOT NULL,
+   contrasena TEXT NOT NULL,
+   nombre_s TEXT NOT NULL,
+   apellido_s TEXT NOT NULL,
+   especialidad TEXT,
    flag_contrasena boolean NOT NULL
 );
 
 CREATE TABLE paciente (
-   id_paciente varchar(9) NOT NULL PRIMARY KEY,
-   nombre_s varchar(255) NOT NULL,
-   apellido_s varchar(255) NOT NULL,
-   edad int NOT NULL,
-   sexo int NOT NULL,
-   contacto int,
-   contacto_emergencia  int,
-   direccion varchar(255),
-   tipo_sangre  int,
-   anotaciones varchar(255)
+   id_paciente INTEGER PRIMARY KEY AUTOINCREMENT,
+   nombre_s TEXT NOT NULL,
+   apellido_s TEXT NOT NULL,
+   edad INTEGER NOT NULL,
+   sexo INTEGER NOT NULL,
+   contacto INTEGER,
+   contacto_emergencia  INTEGER,
+   direccion TEXT,
+   tipo_sangre  INTEGER,
+   anotaciones TEXT
 );
 
 CREATE TABLE diagnostico_medico (
-   id_diagnostico SERIAL PRIMARY KEY,
-   id_cuenta SERIAL NOT NULL,
-   id_paciente varchar(9) NOT NULL,
-   diagnostico varchar(1000) NOT NULL,
-   informacion_adicional varchar(500),
-   tratamiento char(1000),
-   fecha_diagnostico date NOT NULL,
-   CONSTRAINT fk_paciente FOREIGN KEY(id_paciente) REFERENCES paciente (id_paciente),
-   CONSTRAINT fk_medico FOREIGN KEY(id_cuenta) REFERENCES cuenta (id_cuenta)
+   id_diagnostico INTEGER PRIMARY KEY,
+   id_cuenta INTEGER NOT NULL,
+   id_paciente TEXT NOT NULL,
+   diagnostico TEXT NOT NULL,
+   informacion_adicional TEXT,
+   tratamiento TEXT,
+   fecha_diagnostico TEXT NOT NULL,
+   FOREIGN KEY(id_paciente) REFERENCES paciente (id_paciente),
+   FOREIGN KEY(id_cuenta) REFERENCES cuenta (id_cuenta)
 );
 
 
-INSERT INTO cuenta(id_cuenta, correo, contrasena, nombre_s,apellido_s,especialidad,flag_contrasena)
+INSERT INTEGERO cuenta(id_cuenta, correo, contrasena, nombre_s,apellido_s,especialidad,flag_contrasena)
 VALUES(0,'admin@admin.cl','admin1234','admin','admin','admin',TRUE),
       (1,'medico1@medico1.cl','medico1234','medico1','medico1','Neurocirujano',FALSE);
