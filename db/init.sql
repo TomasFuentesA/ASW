752fe8502b5474f1cebf0d66707a3215194f3598
niyetsin <camilo.rubilar@mail.udp.cl>
2022-12-09 22:39:50 -0300

    creacion db

---
+++ db/init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE [IF NOT EXISTS] cuenta (
+   id_cuenta datatype(length) NOT NULL,
+   correo datatype(length) NOT NULL,
+   contrasena datatype(length) NOT NULL,
+   column2 datatype(length) NOT NULL,
+   column3 datatype(length) NOT NULL,
+   column2 datatype(length) NOT NULL,
+   table_constraints
+);
+CREATE TABLE [IF NOT EXISTS] diagnostico_medico (
+   column1 datatype(length) column_contraint,
+   column2 datatype(length) column_contraint,
+   column3 datatype(length) column_contraint,
+   table_constraints
+);
+CREATE TABLE [IF NOT EXISTS] paciente (
+   column1 datatype(length) column_contraint,
+   column2 datatype(length) column_contraint,
+   column3 datatype(length) column_contraint,
+   table_constraints
+);
